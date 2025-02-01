e091902fbcf60430afe22e662c13513a597bccb1
Nesh Kimathi <munenekimathig@gmail.com>
2025-02-01 18:50:36 +0300

    Detect when user makes a duplicate account

--- src/main/java/com/bankingsystem/services/AccountService.java
+++ src/main/java/com/bankingsystem/services/AccountService.java
@@ -3,6 +3,7 @@ package com.bankingsystem.services;
 import com.bankingsystem.models.Account;
 import com.bankingsystem.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -22,8 +23,14 @@ public class AccountService extends Account {
         return accountRepository.findByAccountNumber(accountNumber);
     }
     public String createAccount(Account account) {
-        accountRepository.save(account);
-        return "Account successfully created";
+        try {
+            accountRepository.save(account);
+            return "Account successfully created";
+        } catch (DataAccessException e) {
+            return "Sorry! Account already exists";
+        } catch (Error error) {
+            return "Apologies! Cannot make an account at the moment";
+        }
     }
     public void deleteAccount(Long id) {
         accountRepository.deleteById(id);
